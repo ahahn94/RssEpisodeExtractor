35135fecb5f07354defd8c59e0c8091233960964
ahahn94 <ahahn94@outlook.com>
2018-02-11 01:25:08 +0100

    Add PyGTK GUI
    Add a graphical user interface based on PyGTK and Glade
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- remove.sh
+++ remove.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 python3 setup.py install --record files.txt
 cat files.txt | xargs rm -rf
-rm /usr/bin/rssepisodeextractor
+rm /usr/bin/rssepisodeextractor /usr/bin/rssepisodeextractor-gtk
