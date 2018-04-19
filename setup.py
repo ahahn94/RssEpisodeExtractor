d63d385e5393e603884d5ada50babd97b5566af4
ahahn94 <ahahn94@outlook.com>
2018-04-19 19:50:10 +0200

    Bugfix: Add Feedparser to setup.py
    Add feedparser to setup.py to fix a bug where it was missing and the program would not start.
    
    Signed-off-by: ahahn94 <ahahn94@outlook.com>

--- setup.py
+++ setup.py
@@ -11,5 +11,5 @@ setup(
     author='ahahn94',
     author_email='ahahn94@outlook.com',
     description='Get a list of links to the episodes of a podcast which uses an rss feed with pagination.',
-    install_requires=['gi'],
+    install_requires=['gi', 'feedparser'],
 )
